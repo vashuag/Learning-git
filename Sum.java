8214916e77c26172a72682136e4320220934aa29
Vashu Agarwal <vashuag9@gmail.com>
2021-09-08 01:46:53 +0530

    added message

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void main (String[] args){
         int a= 5;
         int b= 6;
-        System.out.println("The sum"a+b);
+        System.out.println(a+b);
     }
 }
